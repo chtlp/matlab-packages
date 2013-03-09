5567b34d98a3d02d43c4d27c8e37495eb4b47d76
Linpeng Tang <chnttlp@gmail.com>
2013-03-09 15:48:22 -0500

    startup.m

---
+++ startup.m
@@ -0,0 +1,11 @@
+[p, ~, ~] = fileparts(mfilename('fullpath'))
+
+addpath(fullfile(p, 'bgamv120'))
+addpath(fullfile(p, 'matlab'))
+
+addpath(fullfile(p, 'cvx'))
+addpath(fullfile(p, 'cvx/structures'))
+addpath(fullfile(p, 'cvx/lib'))
+addpath(fullfile(p, 'cvx/functions'))
+addpath(fullfile(p, 'cvx/commands'))
+addpath(fullfile(p, 'cvx/builtins'))
