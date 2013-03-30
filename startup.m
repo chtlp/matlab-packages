844c5ef0c0b42f1bcf36b74dda025c72597804c1
Linpeng Tang <chnttlp@gmail.com>
2013-03-30 15:28:01 -0400

    vector lab

--- startup.m
+++ startup.m
@@ -2,6 +2,10 @@
 
 addpath(fullfile(p, 'bgamv120'))
 addpath(fullfile(p, 'matlab'))
+addpath(fullfile(p, 'mtimesx'))
+addpath(fullfile(p, 'baxfun'))
+addpath(fullfile(p, 'Multiprod_2009'))
+addpath(fullfile(p, 'Vector_algebra_2009'))
 
 addpath(fullfile(p, 'cvx'))
 addpath(fullfile(p, 'cvx/structures'))
